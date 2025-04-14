5a9381e9d6489e3ab0b174558ba7a8a58388d65f
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-04-13 22:15:33 -0400

    Setup root component with state and props structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
 import Gallery from './components/Gallery'
 
 function App() {
-
-//const [tours, setTours] = useState([]);
-
-
   return (
     <Gallery></Gallery>
   )
 }
 
-export default App 
+export default App
